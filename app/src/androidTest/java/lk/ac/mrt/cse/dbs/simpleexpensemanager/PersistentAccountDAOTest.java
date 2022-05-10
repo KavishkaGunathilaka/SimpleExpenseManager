84f20a02508652fe1b96781d35cdabcf19f210ad
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-05-10 23:31:18 +0530

    More test cases implemented.

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/PersistentAccountDAOTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/PersistentAccountDAOTest.java
@@ -10,12 +10,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.List;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(JUnit4.class)
 @SmallTest
@@ -41,7 +45,7 @@ public class PersistentAccountDAOTest {
     @Test
     public void testAddAccount() throws InvalidAccountException {
         String accNo = "56789Z";
-        String bankName = "Ja Ja Bank";
+        String bankName = "Jar Jar Bank";
         String name = "Luke Skywalker";
         double balance = 99999.0;
         Account dummyAcct = new Account(accNo, bankName, name, balance);
@@ -56,6 +60,44 @@ public class PersistentAccountDAOTest {
         assertEquals(bankName, acc.getBankName());
         assertEquals(name, acc.getAccountHolderName());
         assertEquals(balance, acc.getBalance());
+
+    }
+
+
+    @Test
+    public void testGetAccountsList() {
+        Account dummyAcct1 = new Account("56789Z", "Jar Jar Bank", "Luke Skywalker", 99999.0);
+        Account dummyAcct2 = new Account("76590V", "Vader Bank", "Yoda", 7467.0);
+
+        persistentAccountDAO.addAccount(dummyAcct1);
+        persistentAccountDAO.addAccount(dummyAcct2);
+
+        List<Account> accountList = persistentAccountDAO.getAccountsList();
+
+        assertFalse(accountList.isEmpty());
+        assertEquals(2, accountList.size());
+
+        assertEquals(dummyAcct1.getAccountNo(), accountList.get(0).getAccountNo());
+        assertEquals(dummyAcct2.getAccountNo(), accountList.get(1).getAccountNo());
+
+    }
+
+    @Test
+    public void testGetAccountNumbersList() {
+
+        Account dummyAcct1 = new Account("56789Z", "Jar Jar Bank", "Luke Skywalker", 99999.0);
+        Account dummyAcct2 = new Account("76590V", "Vader Bank", "Yoda", 7467.0);
+
+        persistentAccountDAO.addAccount(dummyAcct1);
+        persistentAccountDAO.addAccount(dummyAcct2);
+
+        List<String> accountNumbersList = persistentAccountDAO.getAccountNumbersList();
+
+        assertFalse(accountNumbersList.isEmpty());
+        assertEquals(2, accountNumbersList.size());
+
+        assertEquals(dummyAcct1.getAccountNo(), accountNumbersList.get(0));
+        assertEquals(dummyAcct2.getAccountNo(), accountNumbersList.get(1));
 
     }
 }
